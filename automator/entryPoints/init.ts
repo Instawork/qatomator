857859dbd44ab511d9aba25425f86b68c44649fe
Joseph Chu <jchu@instawork.com>
2024-06-09 14:21:42 +0800

    chore: Find root directory and loggings

--- automator/entryPoints/init.ts
+++ automator/entryPoints/init.ts
@@ -1,38 +1,39 @@
-// export const checkSetup = async () => {
-//     if (!process.env.OPENAI_API_KEY) {
-//         console.error('OPENAI_API_KEY Env Var not configured')
-//         throw new Error('OPENAI_API_KEY Env Var not configured')
-//     }
-// }
-// import logger from '../logger'
-// import {resolve} from "path";
-// import fs from "fs";
+import logger from '../logger'
+import fs from 'fs'
+import path from 'path'
+import findRoot from 'find-root'
+
+export const checkSetup = async () => {
+    if (!process.env.OPENAI_API_KEY) {
+        logger.error('OPENAI_API_KEY Env Var not configured')
+        throw new Error('OPENAI_API_KEY Env Var not configured')
+    }
+}
+
+export const downloadDir = path.resolve(findRoot(__dirname), 'reports')
+if (!fs.existsSync(downloadDir)) {
+    fs.mkdirSync(downloadDir, { recursive: true })
+}
+logger.info(downloadDir)
+
+// const prefs = new logging.Preferences()
+// prefs.setLevel(logging.Type.CLIENT, logging.Level.ALL)
 //
-// logger.info('Hello world')
+// const options = new chrome.Options()
+//     // .addArguments('--headless=new')
+//     .addArguments('--disable-gpu')
+//     .addArguments('--no-sandbox')
+//     .addArguments('--disable-dev-shm-usage')
+//     .addArguments('--load-extension=extension/build')
+//     .windowSize({ width: 1440, height: 900 })
+//     .addArguments('--silent-debugger-extension-api')
+//     .setUserPreferences({
+//         'download.prompt_for_download': false,
+//         'download.default_directory': downloadDir,
+//         'download.directory_upgrade': true,
+//     })
+// // .setLoggingPrefs(prefs)
 //
-//     const downloadDir = resolve(__dirname, '../../', 'reports/downloads')
-//     if (!fs.existsSync(downloadDir)) {
-//         fs.mkdirSync(downloadDir, { recursive: true })
-//     }
+// options.setLoggingPrefs(prefs)
 //
-//     let prefs = new logging.Preferences()
-//     prefs.setLevel(logging.Type.CLIENT, logging.Level.ALL)
-//
-//     let options = new chrome.Options()
-//         // .addArguments('--headless=new')
-//         .addArguments('--disable-gpu')
-//         .addArguments('--no-sandbox')
-//         .addArguments('--disable-dev-shm-usage')
-//         .addArguments('--load-extension=extension/build')
-//         .windowSize({ width: 1440, height: 900 })
-//         .addArguments('--silent-debugger-extension-api')
-//         .setUserPreferences({
-//             'download.prompt_for_download': false,
-//             'download.default_directory': downloadDir,
-//             'download.directory_upgrade': true,
-//         })
-//     // .setLoggingPrefs(prefs)
-//
-//     options.setLoggingPrefs(prefs)
-//
-//     let driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build()
+// const driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build()
--- automator/logger.ts
+++ automator/logger.ts
@@ -2,7 +2,7 @@ import winston from 'winston'
 
 const logger = winston.createLogger({
     level: process.env.LOG_LEVEL || 'info',
-    format: winston.format.json(),
+    format: winston.format.combine(winston.format.cli()),
     transports: [new winston.transports.Console()],
     exceptionHandlers: [new winston.transports.File({ filename: 'exception.log' })],
     rejectionHandlers: [new winston.transports.File({ filename: 'rejections.log' })],
