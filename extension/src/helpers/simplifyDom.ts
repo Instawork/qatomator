6abd8be753641c93e0605f7caf1cd61f0e618c3b
Joseph Chu <jchu@instawork.com>
2024-06-21 15:26:22 +0800

    qatomate

--- extension/src/helpers/simplifyDom.ts
+++ extension/src/helpers/simplifyDom.ts
@@ -3,7 +3,9 @@ import { truthyFilter } from './utils'
 
 export async function getSimplifiedDom() {
     const fullDom = await callRPC('getAnnotatedDOM', [], 3)
-    if (!fullDom) return null
+    if (!fullDom) {
+        return null
+    }
 
     const dom = new DOMParser().parseFromString(fullDom, 'text/html')
 
